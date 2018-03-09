3e1446d4549410dba0e694a81b56154b681c4351
Kanodia Adarsh <kanoda@microsoft.com>
2018-03-08 17:17:43 -0800

    Implementation start

---
+++ DurableTaskFrameworkPrototype/LetterCountOrchestration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurableTaskFrameworkPrototype
+{
+	using DurableTask.Core;
+
+	public class LetterCountOrchestration : TaskOrchestration<int [], string>
+	{
+		public override Task<int[]> RunTask(OrchestrationContext context, string input)
+		{
+			input = input.ToLower();
+			foreach(char c in input)
+			{
+				context.ScheduleTask<int>(typeof(AddTask), c);
+			}
+		}
+	}
+}
